5849a589cf6b428d960551cb97351402c2d2eec1
unknown <kampaxton88@gmail.com>
2023-02-15 14:01:41 -0500

    Initial commit

---
+++ src/TheatreKiosk.java
@@ -0,0 +1,12 @@
+public class TheatreKiosk {
+
+    public static void main(String[] args) {
+        int age = 13;
+        if (age>=21)
+        {
+            System.out.println("You get a Wristband!");
+        }
+            else;
+        }
+    }
+
